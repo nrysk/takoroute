b7178d74aa36166e6697e2d79f543eb4b244866b
nrysk <88011751+nrysk@users.noreply.github.com>
2026-05-11 23:56:14 +0900

    feat: ShopCardに距離を掲載

--- app/_components/shop-card.tsx
+++ app/_components/shop-card.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Link from "next/link";
 import type { ComponentPropsWithoutRef } from "react";
-import { cn } from "../_lib/utils";
+import { cn, formatDistance } from "../_lib/utils";
 
 interface ShopCardProps extends ComponentPropsWithoutRef<"div"> {
   id: string;
   name: string;
   access: string;
+  distance?: number;
   photoUrl: string;
 }
 
@@ -15,6 +16,7 @@ export function ShopCard({
   name,
   access,
   photoUrl,
+  distance,
   className,
   ...rest
 }: ShopCardProps) {
@@ -26,13 +28,21 @@ export function ShopCard({
       )}
       {...rest}
     >
-      <Image
-        src={photoUrl}
-        alt={`${name}の写真`}
-        width={80}
-        height={80}
-        className="size-20 object-cover rounded-sm ring-1 ring-black"
-      />
+      <div className="flex flex-col">
+        <Image
+          src={photoUrl}
+          alt={`${name}の写真`}
+          width={80}
+          height={80}
+          className="size-20 object-cover rounded-sm ring-1 ring-black"
+        />
+        {distance && (
+          <span className="text-sm text-gray-500 mt-0.5 font-bold">
+            <span className="text-xs"> ここから </span>
+            {formatDistance(distance)}
+          </span>
+        )}
+      </div>
       <div className="flex flex-col flex-1 gap-0.5">
         <h3 className="text-lg font-semibold">{name}</h3>
 
