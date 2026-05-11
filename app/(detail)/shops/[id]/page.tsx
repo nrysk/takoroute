a6db79d1db1547b19a69e2042425686b8ddda377
Ryosuke <88011751+nrysk@users.noreply.github.com>
2026-05-11 16:07:58 +0900

    feat: 詳細画面の実装 (#12)

---
+++ app/(detail)/shops/[id]/page.tsx
@@ -0,0 +1,67 @@
+import Image from "next/image";
+import { notFound, redirect } from "next/navigation";
+import { BackFab } from "@/app/_components/back-fab";
+import { fetchShopById } from "@/app/_lib/hotpepper";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id?: string }>;
+}) {
+  const { id } = await params;
+
+  if (!id) {
+    redirect("/search");
+  }
+
+  const shop = await fetchShopById(id);
+
+  if (!shop) {
+    notFound();
+  }
+
+  const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(shop.address)}`;
+
+  return (
+    <div className="flex flex-col items-start justify-center min-h-svh gap-4 max-w-lg mx-auto p-2">
+      <h1 className="text-2xl font-bold">{shop.name}</h1>
+      <Image
+        src={shop.photo.pc.l}
+        alt={`${shop.name}の写真`}
+        width={400}
+        height={400}
+        className="object-contain rounded-lg  ring-1 ring-black self-center"
+      />
+      <div className="flex flex-row">
+        <p className="text-nowrap font-bold ">アクセス：</p>
+        <p className="">{shop.access}</p>
+      </div>
+      <div className="flex flex-row">
+        <p className="text-nowrap font-bold">住所　　：</p>
+        <a
+          href={googleMapsUrl}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-500 underline"
+        >
+          {shop.address}
+        </a>
+      </div>
+      <div className="flex flex-row">
+        <p className="text-nowrap font-bold">営業時間：</p>
+        <p className="">{shop.open}</p>
+      </div>
+      <div className="flex flex-row">
+        <p className="text-nowrap font-bold">定休日　：</p>
+        <p className="">{shop.close}</p>
+      </div>
+
+      {/* <p className="">{shop.access}</p>
+      <p className="">{shop.address}</p>
+      <p className="">営業時間: {shop.open}</p>
+      <p className="">定休日　: {shop.close}</p> */}
+
+      <BackFab />
+    </div>
+  );
+}
