f088a5a569800e6727bd104448e7f67ea200f0b4
nrysk <88011751+nrysk@users.noreply.github.com>
2026-05-11 20:08:26 +0900

    feat: 検索結果が0件の場合の画面を追加

--- app/(main)/search/results/page.tsx
+++ app/(main)/search/results/page.tsx
@@ -2,6 +2,7 @@ import { redirect } from "next/navigation";
 import { Pagination } from "@/app/_components/pagination";
 import { ShopCard } from "@/app/_components/shop-card";
 import { fetchTakoyakiShops } from "@/app/_lib/hotpepper";
+import Link from "next/link";
 
 export default async function Page({
   searchParams,
@@ -18,9 +19,35 @@ export default async function Page({
 
   const { shop: shops, results_available } = response.results;
 
+  // 結果が0件の場合は、検索条件を変更するよう促すメッセージを表示
+  if (results_available === 0) {
+    return (
+      <div className="flex flex-col items-center justify-center gap-2">
+        <h2 className="text-2xl font-bold ">
+          近くにたこ焼き屋さんが見つかりませんでした。
+          <br />
+          検索条件を変更して再度お試しください。
+        </h2>
+        {/* 半径が最大ではない場合は、1段階上げるLinkを表示 */}
+        {range === "5" ? (
+          <Link href="/search" className="text-blue-500 hover:underline">
+            検索条件を変更する
+          </Link>
+        ) : (
+          <Link
+            href={`/search/results?lat=${lat}&lon=${lon}&range=${parseInt(range, 10) + 1}&start=1`}
+            className="text-blue-500 hover:underline"
+          >
+            半径を広げて再検索する
+          </Link>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col items-center justify-center gap-2">
-      <h1 className="text-2xl font-bold">検索結果</h1>
+      <h2 className="text-2xl font-bold">検索結果</h2>
       <Pagination
         total={results_available}
         current={parseInt(start || "1", 10)}
