db36997dc2595e42b57a7da76f359b671c36593c
Ryosuke <88011751+nrysk@users.noreply.github.com>
2026-05-12 13:46:01 +0900

    feat: ルートページにクレジット表示 (#25)
    * feat: ルートページにクレジット表示
    
    * fix: ビルドエラーの解消

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -5,6 +7,23 @@ export default function Home() {
       <p className="mt-4 text-lg font-accent">
         Find the best takoyaki spots in town.
       </p>
+
+      <Link href="/search" className="mt-6">
+        始める
+      </Link>
+
+      <a href="http://webservice.recruit.co.jp/">
+        {/** biome-ignore lint/performance/noImgElement: クレジット表示のため */}
+        <img
+          src="http://webservice.recruit.co.jp/banner/hotpepper-m.gif"
+          alt="ホットペッパーグルメ Webサービス"
+          width="88"
+          height="35"
+          // border="0"
+          title="ホットペッパーグルメ Webサービス"
+          className="border-0"
+        />
+      </a>
     </div>
   );
 }
