cd3f4ecfd56d34775ec643486d3e8a218c4208fd
nrysk <88011751+nrysk@users.noreply.github.com>
2026-05-12 11:14:41 +0900

    feat: ルートページにクレジット表示

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
@@ -5,6 +7,22 @@ export default function Home() {
       <p className="mt-4 text-lg font-accent">
         Find the best takoyaki spots in town.
       </p>
+
+      <Link href="/search" className="mt-6">
+        始める
+      </Link>
+
+      <a href="http://webservice.recruit.co.jp/">
+        {/** biome-ignore lint/performance/noImgElement: クレジット掲載方法に従っているため */}
+        <img
+          src="http://webservice.recruit.co.jp/banner/hotpepper-s.gif"
+          alt="ホットペッパーグルメ Webサービス"
+          width="135"
+          height="17"
+          border="0"
+          title="ホットペッパーグルメ Webサービス"
+        />
+      </a>
     </div>
   );
 }
